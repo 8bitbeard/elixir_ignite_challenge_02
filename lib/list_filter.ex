8716b1c64cb46f94c11b7cf06bd302cebde5174f
Wilton Souza <willsouzafilho@gmail.com>
2022-01-28 22:21:28 -0300

    Adding unit tests and improoving function

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,27 @@
 defmodule ListFilter do
   require Integer
 
+  def call([]), do: 0
+
   def call(list) do
     list
     |> Enum.map(fn elem -> Integer.parse(elem) end)
     |> Enum.map(fn elem -> transform_to_digit(elem) end)
     |> Enum.filter(fn elem -> Integer.is_odd(elem) end)
-    |> Enum.reduce(fn _x, acc -> acc + 1 end)
+    |> sum()
   end
 
   defp transform_to_digit(elem) do
     case elem do
-      { num, _ } -> num
+      {num, _} -> num
       :error -> 0
+    end
+  end
+
+  defp sum(list) do
+    case list do
+      [] -> 0
+      _ -> Enum.reduce(list, fn _x, acc -> acc + 1 end)
     end
   end
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,33 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  test "should return the amount of odd numbers in the list" do
+    list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+    response = ListFilter.call(list)
+
+    expected_response = 3
+
+    assert response === expected_response
+  end
+
+  test "should return 0 when passing an empty list" do
+    list = []
+
+    response = ListFilter.call(list)
+
+    expected_response = 0
+
+    assert response === expected_response
+  end
+
+  test "should return 0 when passing an list without any number" do
+    list = ["a", "b", "c", "d"]
+
+    response = ListFilter.call(list)
+
+    expected_response = 0
+
+    assert response === expected_response
   end
 end
